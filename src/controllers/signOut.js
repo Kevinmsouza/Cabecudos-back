b5a14fe3f11d3216c69366fec73ccd74bb7619f8
leandrodcs <eng.leandrodcs@gmail.com>
2021-11-09 13:53:38 -0300

    Sign-out route fully functional

--- src/controllers/signOut.js
+++ src/controllers/signOut.js
@@ -1,10 +1,12 @@
-import connection from '../database/database';
+import connection from '../database/database.js';
 
 export default async function signOut(req, res) {
-    const token = req.headers.authorization?.replace('Bearer ', '');
+    const token = req.headers.authorization?.split('Bearer ')[1];
 
     try {
         await connection.query('DELETE FROM sessions WHERE token = $1', [token]);
+        console.log("foi")
+        res.sendStatus(200);
     } catch (error) {
         // eslint-disable-next-line no-console
         console.log(error);
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,9 +1,11 @@
 /* eslint-disable comma-dangle */
-import connection from '../database/database';
+import connection from '../database/database.js';
 
 export default async function checkToken(req, res, next) {
-    const token = req.headers.authorization?.replace('Bearer ', '');
+    console.log("xau")
+    const token = req.headers.authorization?.split('Bearer ')[1];
+    console.log(token)
     const result = await connection.query('SELECT * FROM sessions WHERE token = $1', [token]);
-    if (result.rows.length) return res.sendStatus(401);
+    if (!result.rows.length) return res.sendStatus(401);
     next();
 }
